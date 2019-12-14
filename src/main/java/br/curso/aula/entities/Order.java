5ba709bb4d10e838a4929683bc0a948eca422857
verasouza <vera.al27@gmail.com>
2019-12-13 21:39:27 -0300

    OrderStatus enum

--- src/main/java/br/curso/aula/entities/Order.java
+++ src/main/java/br/curso/aula/entities/Order.java
@@ -13,6 +13,8 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import br.curso.aula.entities.enums.OrderStatus;
+
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
@@ -27,17 +29,25 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	private Integer orderStatus;
 
 	public Order() {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	
+
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client ) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
+		
 	}
+
+
 
 	public Long getId() {
 		return id;
@@ -61,6 +71,16 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
